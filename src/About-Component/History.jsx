a7da2e86fc069962b286baa9eae1751e979becb8
Emmanuel Great <greatema03@gmail.com>
2026-05-22 09:28:14 +0100

    updated history

--- src/About-Component/History.jsx
+++ src/About-Component/History.jsx
@@ -14,18 +14,7 @@ function History(){
                 </div>
             <div className="max-w-[700px]">
                 <p className="text-md leading-relaxed">
-                    Rev Willy Bigman Enoch! . Was born with a passion for God, 
-                    as a child he loved the things of God so much, at the age of twelve years 
-                    he gave his life and will to God to protect and use him, Willy B is from Nigeria Rivers State,
-                    Abua Odual . L.G.A. Emago Kugbo.. Date of Birth. 1980 February 14th. He is married 
-                    and they're bless with Children. Presently he is a Pastor in Christian 
-                    Pentecostal Mission Int't. He base in Lagos Nigeria He is an Evangelist by calling with a teaching grace,
-                    he came into the full-time ministry in the year 1999, He planted a Church in Omoku Onelga Rivers State Nigeria 
-                    1997 and Successful Pastor them for twelve years Christian Pentecostal Mission Int't, and was move 
-                    to OYigbo in Rivers State Nigeria,  He pastor them for nine months and was moved to Lagos CPM Ilasa branch, 
-                    waiting for the next move. Willy B is a Man God has shown mercy, how my journey started, in the year 1993 I had a passion for ministry as a young boy. 
-                    I gathered children in the street and I started preaching the gospel to them. The first time I handle mic in a church to preach, that's was awesome, I saw a Man went down under the Anointing,  
-                    as I was praying that was how my father believed in me. 1997 In the year 1999  I came to Lagos and joined a Bible School. (P. I. B. S.) Pentecostal International Bible S.
+                    Rev Willy Bigman Enoch. Is a Carry of Grace, ordained to preach the Gospel with the heart of love and compassion call as an Evangelist, with a prophetic grace, a Preacher and a Teacher of the Word, He is ordained minister with Christian Pentecostal Mission Int't, Born in 14th February 1980. From Abua Odual Local Government Area, Emago Kugbo, Rivers State, Nigeria. Married to Mrs Hope Willy Enoch and they are blessed with children.
                 </p>
                 </div>
             </div>   
